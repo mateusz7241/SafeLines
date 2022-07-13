e422192cc194324213636a30f4953363932b8a78
Mateusz <mateusz7241@gmail.com>
2022-07-13 17:14:08 +0200

    Rejestracja do bazy danych Firebase

--- app/src/main/java/com/example/currentlatlon/RegisterActivity.java
+++ app/src/main/java/com/example/currentlatlon/RegisterActivity.java
@@ -1,40 +1,113 @@
 package com.example.currentlatlon;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class RegisterActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class RegisterActivity extends AppCompatActivity{
 
     Button registerButton2;
-    EditText registerLogin,registerPassword,registerConfirmPassword;
+    EditText registerFullName,registerEmail,registerPassword;
+    private FirebaseAuth mAuth;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        registerLogin = findViewById(R.id.registerLogin);
+        registerFullName = findViewById(R.id.registerFullName);
+        registerEmail = findViewById(R.id.registerEmail);
         registerPassword = findViewById(R.id.registerPassword);
-        registerConfirmPassword = findViewById(R.id.registerConfirmPassword);
         registerButton2 = findViewById(R.id.registerButton2);
+
+        //instancja bazy danych
+        mAuth = FirebaseAuth.getInstance();
+
+
 
         registerButton2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(registerLogin.getText().toString().isEmpty() || registerPassword.getText().toString().isEmpty() || registerConfirmPassword.getText().toString().isEmpty()){
-                    Toast.makeText(RegisterActivity.this, "Wypełnij wszystkie pola", Toast.LENGTH_SHORT).show();
-                }else if(registerLogin.getText().toString().length() > 1 && registerPassword.getText().toString() == registerConfirmPassword.getText().toString()){
-                    Toast.makeText(RegisterActivity.this, "Pomyślnie zarejestrowano", Toast.LENGTH_SHORT).show();
-                    //String login = registerLogin.getText().toString();
-                    //String pass = registerPassword.getText().toString();
-                    // mozna wyslac ten login i haslo do bazy danych lokalnej lub firebase
+                registerUser();
+            }
+        });
+    }
+
+
+    private void registerUser(){
+        String fullName = registerFullName.getText().toString().trim();
+        String email = registerEmail.getText().toString().trim();
+        String pass = registerPassword.getText().toString().trim();
+
+        if(fullName.isEmpty()){
+            registerFullName.setError("Wpisz imie i nazwisko");
+            registerFullName.requestFocus();
+            return;
+        }
+
+        if(email.isEmpty()){
+            registerEmail.setError("Wpisz email");
+            registerEmail.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            registerEmail.setError("Wpisz poprawny adres mail");
+            registerEmail.requestFocus();
+            return;
+        }
+        if(pass.isEmpty()){
+            registerPassword.setError("Wpisz hasło");
+            registerPassword.requestFocus();
+            return;
+        }
+        if(pass.length() < 6){
+            registerPassword.setError("Hasło składające się z minimum 6 znaków");
+            registerPassword.requestFocus();
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    User user = new User(fullName,email);
+
+                    FirebaseDatabase.getInstance().getReference("Users").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                          if(task.isSuccessful()){
+                              Toast.makeText(RegisterActivity.this, "Rejestracja przebiegła pomyślnie", Toast.LENGTH_SHORT).show();
+                          }else{
+                              Toast.makeText(RegisterActivity.this, "Coś poszło nie tak! Spróbuj ponownie!", Toast.LENGTH_LONG).show();
+                          }
+                        }
+                    });
+
+                }else{
+                    Toast.makeText(RegisterActivity.this, "Coś poszło nie tak! Spróbuj ponownie!", Toast.LENGTH_LONG).show();
                 }
             }
         });
     }
+
+
+    //Wylogowanie
+    //FirebaseAuth.getInstance().signOut();
+
+
 }
---
+++ app/src/main/java/com/example/currentlatlon/User.java
@@ -0,0 +1,15 @@
+package com.example.currentlatlon;
+
+public class User {
+
+    public String fullName, email;
+
+    User(){
+    }
+    public User(String fullName,String email){
+        this.fullName = fullName;
+        this.email = email;
+    }
+
+
+}
